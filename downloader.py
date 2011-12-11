d464638fd0389f54d780f61f8bacb6551808312a
alice <genocidedragon@gmail.com>
2011-12-11 21:49:27 +0900

    add version number, add logging process, and bug fixed.

--- downloader.py
+++ downloader.py
@@ -4,7 +4,10 @@
 import os
 import sys
 import urllib
+import datetime
 from BeautifulSoup import BeautifulSoup
+
+print "Image Downloader ver 1.0.3"
 
 try:
 	url = sys.argv[1]
@@ -13,6 +16,23 @@ except:
 	print "$ downloader.py [url] [directory]"
 	sys.exit(1)
 
+print "Donwload URL : %s" % url
+
+# logging
+print "logging..."
+now_datetime = datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
+log_file = os.environ["HOME"]+"/image-downloader.log"
+dir = os.getcwd() if directory == "." else os.path.normpath(os.path.join(os.getcwd(),directory))
+if os.access(log_file, os.F_OK):
+	fp = open(log_file,"a+")
+else:
+	fp = open(log_file,"w")
+
+logger = "Date : %s - %s to [%s]\n" % (now_datetime, url, dir)
+fp.write(logger)
+fp.close()
+	
+
 
 # get image urls from url
 
@@ -20,10 +40,11 @@ soup = BeautifulSoup(urllib.urlopen(url).read())
 image_urls = []
 for tag in soup.findAll("a"):
 	if tag.img:
-		ext = os.path.splitext(tag.img["src"])[1]
+		url = tag["href"]
+		ext = os.path.splitext(url)[1]
 		allow_exts = [".jpg",".JPG",".png",".PNG"]
 		if ext in allow_exts:
-			image_urls += [tag.img["src"]]
+			image_urls += [url]
 
 # move directory
 if os.path.isdir(directory):
